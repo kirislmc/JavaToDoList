77b148687f21266008ec4dbdf9f005e94ab1711b
Kirislmc <kirislmc@icloud.com>
2026-08-26 17:13:16 +0200

    Refactor ToDoList to use ArrayList for tasks
    Refactored the ToDoList program to use an ArrayList for dynamic task storage instead of individual variables.

--- ToDoList.java
+++ ToDoList.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-
+import java.util.ArrayList;
 public class ToDoList {
 
     public static void main(String[] args) {
@@ -8,29 +8,24 @@ public class ToDoList {
 
         System.out.println("Welcome to the To do list! ");
 
-        System.out.println("Enter you task: ");
-        String task1 = scanner.nextLine();
+        ArrayList<String> todoList = new ArrayList<>();
 
-        System.out.println("Enter you task: ");
-        String task2 = scanner.nextLine();
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Enter you task: ");
+            String answer = scanner.nextLine();
+            todoList.add(answer);
+        }
 
-        System.out.println("Enter you task: ");
-        String task3 = scanner.nextLine();
 
-        System.out.println("Enter you task: ");
-        String task4 = scanner.nextLine();
 
-        System.out.println("Enter you task: ");
-        String task5 = scanner.nextLine();
+        for (int i = 0; i < todoList.size(); i++ ) {
+            System.out.println((i + 1) + "." + todoList.get(i));
+        }
 
-        System.out.println("Your to do list is: ");
-        System.out.println("1." + task1);
-        System.out.println("2." + task2);
-        System.out.println("3." + task3);
-        System.out.println("4." + task4);
-        System.out.println("5." + task5);
+
 
         scanner.close();
 
     }
+
 }
